e936bd9a288b23124aa44b4375dbfe0809221ade
Heman Ho <heman.ho.lh@gmail.com>
2025-09-29 16:32:26 -0700

    load data, tokenize, train

---
+++ app/app.py
@@ -0,0 +1,44 @@
+from datasets import load_dataset, get_dataset_split_names
+from transformers import AutoTokenizer, AutoModelForSequenceClassification, DataCollatorForSeq2Seq, Seq2SeqTrainer, Seq2SeqTrainingArguments, T5ForConditionalGeneration
+from data import util
+
+# Load tokenizer and model
+checkpoint = "t5-small"
+model = T5ForConditionalGeneration.from_pretrained(checkpoint)
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+# Load the dataset
+dataset_train = load_dataset("newfacade/LeetCodeDataset", split="train")
+dataset_test = load_dataset("newfacade/LeetCodeDataset", split="test")
+
+# apply tokenization in batches 
+print("Tokenizing the dataset...")
+tokenized_train = dataset_train.map(util.preprocess_dataset, batched=True)
+tokenized_test = dataset_test.map(util.preprocess_dataset, batched=True)
+
+
+
+# Set the format of the dataset to be compatible with PyTorch
+print("setting the format...")
+tokenized_train.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+tokenized_test.set_format(type="torch", columns=["input_ids", "attention_mask", "labels"])
+
+print("training the model ...")
+# Set up the training arguments
+training_args = Seq2SeqTrainingArguments(
+    output_dir = "./checkpoints",
+    eval_strategy = "epoch",
+)
+
+data_collator = DataCollatorForSeq2Seq(tokenizer, model=model)
+
+trainer = Seq2SeqTrainer(
+    model=model, 
+    args=training_args,
+    train_dataset=tokenized_train, 
+    eval_dataset=tokenized_test,
+    tokenizer=tokenizer, 
+    data_collator=data_collator
+)
+
+trainer.train()
